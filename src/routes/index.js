8ef80f773d143cfea2484be5670d9290577f70af
namannanu <2024pietcsnaman109@poornima.org>
2025-09-21 16:01:29 +0530

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ const jobRoutes = require('../modules/jobs/job.routes');
 const applicationRoutes = require('../modules/applications/application.routes');
 const attendanceRoutes = require('../modules/attendance/attendance.routes');
 const shiftRoutes = require('../modules/shifts/shift.routes');
+const shiftController = require('../modules/shifts/shift.controller');
+const { protect: protectAuth } = require('../shared/middlewares/auth.middleware');
 const notificationRoutes = require('../modules/notifications/notification.routes');
 const conversationRoutes = require('../modules/conversations/conversation.routes');
 const budgetRoutes = require('../modules/budgets/budget.routes');
@@ -16,6 +18,13 @@ const paymentRoutes = require('../modules/payments/payment.routes');
 const subscriptionRoutes = require('../modules/subscriptions/subscription.routes');
 
 const router = express.Router();
+const shiftsRouter = express.Router();
+
+shiftsRouter.use(protectAuth);
+shiftsRouter.get('/', shiftController.listShifts);
+shiftsRouter.get('/swaps', shiftController.listSwaps);
+shiftsRouter.post('/swaps', shiftController.requestSwap);
+shiftsRouter.patch('/swaps/:swapId', shiftController.updateSwap);
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
@@ -26,6 +35,7 @@ router.use('/jobs', jobRoutes);
 router.use('/applications', applicationRoutes);
 router.use('/attendance', attendanceRoutes);
 router.use('/shift-swaps', shiftRoutes);
+router.use('/shifts', shiftsRouter);
 router.use('/notifications', notificationRoutes);
 router.use('/conversations', conversationRoutes);
 router.use('/budget', budgetRoutes);
